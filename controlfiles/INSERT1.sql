fd1cca474f27870833028971d45320ec3c9b437d
Margaret Atkinson <s1676540@baltic10.geos.ed.ac.uk>
2019-01-31 14:39:58 +0000

    python files created

---
+++ controlfiles/INSERT1.sql
@@ -0,0 +1,24 @@
+INSERT INTO S1217815.DRAGONS (MIGRATION_ID)
+select 1 FROM DUAL
+union
+select 2 FROM DUAL
+union
+select 3 FROM DUAL
+union
+select 4 FROM DUAL
+union
+select 5 FROM DUAL
+union
+select 6 FROM DUAL
+union
+select 7 FROM DUAL
+union
+select 8 FROM DUAL
+union
+select 9 FROM DUAL
+union
+select 10 FROM DUAL
+union
+select 11 FROM DUAL
+union
+select 12 FROM DUAL; 
